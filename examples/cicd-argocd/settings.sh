64d8771907fa20256a57719f9343b46f5b7e2664
marcredhat <mchisine@redhat.com>
2019-10-05 13:03:20 -0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   examples/cicd-argocd/activity-monitor/show-activity-monitor-logs
    	modified:   examples/cicd-argocd/activity-monitor/start-activity-monitor
    	modified:   examples/cicd-argocd/activity-monitor/stop-activity-monitor
    	modified:   examples/cicd-argocd/argocd/get-argocd-browser-login
    	modified:   examples/cicd-argocd/argocd/get-argocd-cmd-line-login
    	modified:   examples/cicd-argocd/argocd/get-argocd-server-url
    	modified:   examples/cicd-argocd/argocd/port-forward-argocd
    	modified:   examples/cicd-argocd/argocd/show-argocd-logs
    	modified:   examples/cicd-argocd/argocd/start-argocd
    	modified:   examples/cicd-argocd/argocd/stop-argocd
    	modified:   examples/cicd-argocd/argocd/update-argocd-webhook-secret
    	modified:   examples/cicd-argocd/create-demo-tmux-session
    	new file:   examples/cicd-argocd/create-demo-tmux-session.orig
    	modified:   examples/cicd-argocd/create-secrets
    	modified:   examples/cicd-argocd/delete-secrets
    	modified:   examples/cicd-argocd/docker-devbox-image-builder
    	modified:   examples/cicd-argocd/docker-devbox-manifest-updater
    	modified:   examples/cicd-argocd/jenkins/get-initial-admin-password
    	modified:   examples/cicd-argocd/jenkins/get-jenkins-browser-login
    	modified:   examples/cicd-argocd/jenkins/get-jenkins-github-webhook-details
    	modified:   examples/cicd-argocd/jenkins/get-jenkins-url
    	modified:   examples/cicd-argocd/jenkins/port-forard-jenkins
    	modified:   examples/cicd-argocd/jenkins/show-jenkins-logs
    	modified:   examples/cicd-argocd/jenkins/start-jenkins
    	modified:   examples/cicd-argocd/jenkins/stop-jenkins
    	modified:   examples/cicd-argocd/k8s-local-docker-registry/port-forward-registry
    	modified:   examples/cicd-argocd/k8s-local-docker-registry/start-docker-private-registry
    	modified:   examples/cicd-argocd/k8s-local-docker-registry/stop-docker-private-registry
    	modified:   examples/cicd-argocd/seldon-core/create-seldon-deployment
    	modified:   examples/cicd-argocd/seldon-core/delete-seldon-deployment
    	modified:   examples/cicd-argocd/seldon-core/port-forard-ambassador
    	modified:   examples/cicd-argocd/seldon-core/port-forard-api-server
    	modified:   examples/cicd-argocd/seldon-core/start-seldon-core
    	modified:   examples/cicd-argocd/seldon-core/stop-seldon-core
    	new file:   examples/cicd-argocd/settings.sh
    	modified:   examples/cicd-argocd/start-all
    	modified:   examples/cicd-argocd/watch-cluster
    	modified:   examples/cicd-argocd/watch-cluster-pods
    	modified:   examples/cicd-argocd/watch-rollout-status
    	typechange: monitoring/prometheus/prometheus-config.yml
    	deleted:    monitoring/prometheus/rules
    	new file:   monitoring/prometheus/rules/cpu-usage.rules
    	new file:   monitoring/prometheus/rules/instance-availability.rules
    	new file:   monitoring/prometheus/rules/low-disk-space.rules
    	new file:   monitoring/prometheus/rules/mem-usage.rules

---
+++ examples/cicd-argocd/settings.sh
@@ -0,0 +1,14 @@
+# leave empty for default context, or use a particular context
+KUBE_CONTEXT=
+
+MODEL_SOURCE_FILES_DIR=/home/demouser/seldon-core/examples/
+K8S_MANIFEST_FILES_DIR=/home/demouser/seldon-core/examples/
+
+GITHUB_USER=
+GITHUB_TOKEN=
+
+ARGOCD_VERSION=v1.2.2
+
+JENKINS_USER_NAME=
+JENKINS_USER_PASSWORD=
+
